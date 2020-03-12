c7357d0153c1ab6e2abae8696aaa7e7d16652ef8
VRLIFE-git <dev@vrlife.cz>
2020-03-12 16:29:43 +0100

    Movement is now relative to the players view

--- Runtime/Scripts/PlayerHandViewModel.cs
+++ Runtime/Scripts/PlayerHandViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace Vrlife.Core.Vr
+﻿using System;
+
+namespace Vrlife.Core.Vr
 {
+    [Serializable]
     public class PlayerHandViewModel
     {
+        public bool triggerClicked;
+
+        public bool onReleaseFired;
+
+        public bool onGrabFired;
+
     }
 }
--- Runtime/Scripts/VrRigControllerProxy.cs
+++ Runtime/Scripts/VrRigControllerProxy.cs
@@ -6,11 +6,24 @@ namespace Vrlife.Core.Vr
     public class VrRigControllerProxy : MonoBehaviour
     {
         [Inject] public IPlayerInputUpdater inputUpdater;
+
+        public Camera _camera;
+        [Range(0f, 2f)]
+        public float movementSpeed = 1f;
+        
         private void Update()
         {
             Vector2 input = inputUpdater.RightHandInputDevice.InteractionInformation.JoystickPosition;
-            Vector3 coordinates = new Vector3(input.x, 0, input.y);
-            transform.position += coordinates * 0.1f;
+
+            Vector3 forward = _camera.transform.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = _camera.transform.right;
+            right.y = 0f;
+            right.Normalize();
+
+            Vector3 coordinates = right * input.x + forward * input.y;
+            transform.Translate(coordinates * movementSpeed * Time.deltaTime);
         }
     }
 }
