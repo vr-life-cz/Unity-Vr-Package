3c7c8b6a2b949a6ceaf324430a7482927aae0f2e
VRLIFE-git <dev@vrlife.cz>
2020-03-12 08:55:10 +0100

    Added VrRig Movement

---
+++ Runtime/Scripts/VrRigControllerProxy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Vrlife.Core.Vr
+{
+    public class VrRigControllerProxy : MonoBehaviour
+    {
+        [Inject] public IPlayerInputUpdater inputUpdater;
+        private void Update()
+        {
+            Vector2 input = inputUpdater.RightHandInputDevice.InteractionInformation.JoystickPosition;
+            Vector3 coordinates = new Vector3(input.x, 0, input.y);
+            transform.position += coordinates * 0.1f;
+        }
+    }
+}
