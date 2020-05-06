19dbaf8d5f508d5fb7a133cc55b845828d9957c6
Pozitrone <jirirkas.skola@gmail.com>
2020-05-06 11:10:48 +0200

    Dummy installer

---
+++ Runtime/Scripts/DummyInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Vrlife.Core;
+using Vrlife.Core.GoogleApis.TextToSpeech;
+using Vrlife.Core.Mvc;
+using Vrlife.Core.Mvc.Implementations;
+using Zenject;
+
+public class DummyInstaller : MonoInstaller<DummyInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.BindViewController<ISimpleView, ISimpleController>()
+            .WithControllerImplementation<SimpleController>()
+            .WithViewModelProcessor<SimpleViewModel, SimpleViewProcessor>();
+
+        Container.Bind<IGoogleTextToSpeechService>().To<GoogleTextToSpeechService>().AsSingle();
+
+        Container.BindInterfacesAndSelfTo<CoroutineProcessor>().FromNewComponentOnNewGameObject().AsSingle();
+
+        Container.BindViewController<ISceneTransitorView, ISceneTransitorController>()
+            .WithControllerImplementation<SceneTransitorController>()
+            .WithViewModelProcessor<SceneTransitorViewModel, SceneTransitorProcessor>();
+    }
+}
