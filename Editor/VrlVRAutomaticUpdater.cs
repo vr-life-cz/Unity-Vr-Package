475df8df12b09ec7ded2ca903f44603dad91c0bd
Pozitrone <jirirkas.skola@gmail.com>
2021-02-25 13:42:50 +0100

    Added automatic updater

---
+++ Editor/VrlVRAutomaticUpdater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager;
+
+namespace Plugins.com.vrlife.vr.Editor
+{
+    public class VrlVRAutomaticUpdater
+    {
+        [InitializeOnLoad]
+        public class AutomaticUpdate
+        {
+            static AutomaticUpdate()
+            {
+                Client.Add("https://github.com/virtual-real-life/Unity-Vr-Package.git");
+            }
+        }
+    }
+}
